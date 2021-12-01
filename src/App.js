0bc24d72055c5b3e57551158a79e92186f0280b6
A B M Shawon Islam <abmshawonislam@gmail.com>
2021-12-02 00:11:21 +0600

    modal and drag upload done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Slide from '@mui/material/Slide';
+
+import Dropzone from 'react-dropzone'
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
 
 function App() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleFile = (acceptedFiles )=>(
+    console.log(acceptedFiles[0].name)
+  )
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    
+          <div>
+      <Button variant="contained" onClick={handleClickOpen}>
+        Slide in alert dialog
+      </Button>
+      <Dialog
+        open={open}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={handleClose}
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <DialogContent>
+          <Dropzone onDrop={acceptedFiles => handleFile(acceptedFiles)}>
+              {({getRootProps, getInputProps}) => (
+                <section>
+                  <div {...getRootProps()}>
+                    <input {...getInputProps()} />
+                    <p style={{padding:"20px",height:"200px",border:"3px dotted #000",borderRadius:"5px"}}>Drag 'n' drop some files here, or click to select files</p>
+                  </div>
+                </section>
+              )}
+          </Dropzone>
+        </DialogContent>
+        <DialogActions>
+          <Button variant="contained" color="error" onClick={handleClose}>Cancel</Button>
+          <Button variant="contained" color="success" onClick={handleClose}>Save</Button>
+        </DialogActions>
+      </Dialog>
     </div>
+{/* ================================== */}
+    
+    </>
   );
 }
 
